b7b25f93807aa1b3f16720be361a5b3bf85b6fe8
jifukui <805013833@qq.com>
2019-06-18 16:18:52 +0800

    添加容器

--- vector/main.cpp
+++ vector/main.cpp
@@ -5,18 +5,47 @@ using namespace std ;
 
 int main()
 {
-        vector< vector<int> > b(10, vector<int>(5)) ;
-        //全部输出
-        int m, n ;
-        int r1=b.size();
-        int r2=b[0].size();
-        cout<<r1<<","<<r2<<endl;
-        for(m=0; m<b.size(); m++)           //b.size()获取行向量的大小
-        {
-            for(n=0; n<b[m].size(); n++)    //获取向量中具体每个向量的大小
-                cout<<b[m][n]<<" " ;
-            cout<<"\n" ;
-        }
+    vector< vector<int> > matrix(2, vector<int>(3)) ;
+    int m, n ;
+    int r1=matrix.size();
+    int r2=matrix[0].size();
+    cout<<r1<<","<<r2<<endl;
+    matrix[0][0]=1;   
+    matrix[0][1]=2;
+    matrix[0][2]=3;
+    matrix[1][0]=4;
+    matrix[1][1]=5;
+    matrix[1][2]=6;    
 
-        return 0;
+
+
+    for(i;i<r1;i++)
+    {
+            
+        if(matrix[i][0]<=target&&matrix[i][r2]>=target)
+        {
+            int mid,height,low;
+            low=0;
+            height=r2;
+            while(low<=height)
+            {
+                mid=(low+height)/2;
+                if(matrix[i][mid]<target)
+                {
+                    low=mid+1;
+                }
+                else if(matrix[i][mid]>target)
+                {
+                    height=mid-1;
+                }
+                else
+                {
+                    count<<"success"<<endl;
+                }           
+            }
+            count<<"error"<<endl;
+        }
+    }
+    count<<"error"<<endl;
+    return 0;
 }
